bd915fbd7356e5c8066bd7c59682ea9b88ed30c3
Roymand <chuyirlee@live.com>
2023-09-20 19:50:24 -0500

    Added model and seeds for Class and Race

---
+++ models/Race.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Race extends Model {}
+
+Race.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'race',
+    }
+  );
+  
+  module.exports = Race;
