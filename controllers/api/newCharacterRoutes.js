d2d3fa728ad00b9392516c98416826b67af664ac
breelovesfrogs <134086243+breelovesfrogs@users.noreply.github.com>
2023-09-20 22:06:24 -0500

    more routes

--- controllers/api/newCharacterRoutes.js
+++ controllers/api/newCharacterRoutes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const { Race, Class } require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Routes for race screen
-router.post('/newCharacter/race', async (req, res) => {
+router.post('api/newCharacter/race', async (req, res) => {
     const newRace = Race.create(req.body)
     await res.json(newRace)
 
 })
+
+// routes for
+router.post
 
 
 
