32c6da1fe98a1d2cabdaeebddfc5587d8baa9c2d
aridel96 <adelgado.162@gmail.com>
2023-09-21 21:02:42 -0400

    Created arrays for class, race, and background

--- public/js/login.js
+++ public/js/login.js
@@ -25,7 +25,7 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#username-signup').value.trim();
+  const name = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
