7195b28e8247ed1b5c6ea8e52063e0f139c03f70
aridel96 <adelgado.162@gmail.com>
2023-09-27 19:55:59 -0400

    Worked on a custom helper for character Form handlebar

---
+++ public/js/createCharacter.js
@@ -0,0 +1,15 @@
+const characterName = document.getElementById("character-name").value.trim();
+const level = document.getElementById("level").value.trim();
+const strength = document.getElementById("strength").value.trim();
+const dexterity = document.getElementById("dexterity").value.trim();
+const constitution = document.getElementById("constitution");
+const intelligence = document.getElementById("intelligence").value.trim();
+const wisdom = document.getElementById("wisdom").value.trim();
+const charisma = document.getElementById("charisma").value.trim();
+const background = document.getElementById("bg-menu").value.trim();
+const characterClass = document.getElementById("class-menu").value.trim();
+const race = document.getElementById("race-menu").value.trim();
+
+const btnEl = document.getElementById("charForm");
+
+btnEl.addEventListener
